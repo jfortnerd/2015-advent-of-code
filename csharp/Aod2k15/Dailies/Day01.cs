c0ea98f1f15a2cae5a5f9533d83c3766c4638511
COS-JF <Jeremiah.Fortner@shreveportla.gov>
2023-11-29 14:15:53 -0600

    Engine and Day 1 Solution

---
+++ csharp/Aod2k15/Dailies/Day01.cs
@@ -0,0 +1,51 @@
+﻿using Jfortnerd.Aod2k15;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aod2k15.Dailies
+{
+    internal class Day01 : Jfortnerd.Aod2k15.Dailies.Day
+    {
+        public override void RunSolution()
+        {
+            int currentPosition = 1;
+            int currentFloor = 0;
+            char ascendFloor = '(';
+            char descendFloor = ')';
+            int basementFirstPosition = -1;
+
+            foreach (char c in Sample)
+            {
+                if (c == ascendFloor)
+                {
+                    currentFloor++;
+                }
+                else if (c == descendFloor)
+                {
+                    currentFloor--;
+
+                    // check if descend into basement
+                    if (currentFloor < 0)
+                    {
+                        if (basementFirstPosition == -1)
+                        {
+                            basementFirstPosition = currentPosition;
+                        }
+                    }
+                }
+
+                currentPosition++;
+            }
+
+            // Part 1 of solution
+            Solution = String.Concat(Solution, "The instructions take Santa to floor: " + currentFloor + "\n");
+
+            Solution = String.Concat(Solution, "The position of the character that causes Santa to first enter the basement is position #: " + basementFirstPosition + "\n");
+
+            Console.Write(Solution);
+        }
+    }
+}
